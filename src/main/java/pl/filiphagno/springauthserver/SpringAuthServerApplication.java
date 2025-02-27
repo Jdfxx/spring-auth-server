7ad28dfaf9b4d9cd592866cb361957fe6d47057b
Filip <fhagno@yahoo.com>
2025-02-27 11:11:51 +0100

    Init commit

---
+++ src/main/java/pl/filiphagno/springauthserver/SpringAuthServerApplication.java
@@ -0,0 +1,13 @@
+package pl.filiphagno.springauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthServerApplication.class, args);
+    }
+
+}
